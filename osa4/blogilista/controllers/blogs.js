2ab0ba70451d204c2ba590ed08d393e7d2745306
Henri Kiiski <toppe@pop-os.localdomain>
2021-08-03 15:13:04 +0300

    4.13-4.14

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -8,12 +8,35 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-  console.log(blog)
   try {
     const result = await blog.save()
     return response.status(201).json(result)
   } catch (exception) {
     return response.status(400).end()
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id)
+    return response.status(204).end()
+  } catch (exception) {
+    return response.status(404).end()
+  }
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const blog = {
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes,
+  }
+  try {
+    const updatedNote = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updatedNote)
+  } catch (exception) {
+    response.status(404).end()
   }
 })
 
--- osa4/blogilista/tests/blogs.test.js
+++ osa4/blogilista/tests/blogs.test.js
@@ -29,65 +29,106 @@ test('check id-field name', async () => {
   expect(allBlogs[0].id).toBeDefined()
 })
 
-test('adding a new blog', async () => {
-  const newBlog = 
+describe('adding blogs', () => {
+  test('adding a new blog', async () => {
+    const newBlog = 
+      { 
+        title: "Poikamiehen herkkuruoat",
+        author: "Anonyymi",
+        url: "https://poikamiehenherkkuruoat.tumblr.com/",
+        likes: 24,
+      }
+    await api.post('/api/blogs')
+             .send(newBlog)
+             .expect(201)
+             .expect('Content-Type', /application\/json/)
+
+    const allBlogs = await helper.blogsInDb()
+    expect(allBlogs).toHaveLength(helper.biglist.length +1)
+    const addedBlog = allBlogs.pop()
+    expect(addedBlog.title).toBe(newBlog.title)
+  })
+
+  test('if no likes, set to zero', async () => {
+    const newBlog = 
     { 
       title: "Poikamiehen herkkuruoat",
       author: "Anonyymi",
       url: "https://poikamiehenherkkuruoat.tumblr.com/",
-      likes: 24,
     }
-  await api.post('/api/blogs')
-           .send(newBlog)
-           .expect(201)
-           .expect('Content-Type', /application\/json/)
+    const response = await api.post('/api/blogs')
+                               .send(newBlog)
+                               .expect(201)
+                               .expect('Content-Type', /application\/json/)
+    expect(response.body.likes).toBe(0)
+  })
 
-  const allBlogs = await helper.blogsInDb()
-  expect(allBlogs).toHaveLength(helper.biglist.length +1)
-  const addedBlog = allBlogs.pop()
-  expect(addedBlog.title).toBe(newBlog.title)
+  test('invalid parameters in adding a new blog', async () => {
+    const noTitle = 
+    { 
+      author: "Anonyymi",
+      url: "https://poikamiehenherkkuruoat.tumblr.com/",
+    }
+    await api.post('/api/blogs')
+             .send(noTitle)
+             .expect(400)
+    const noUrl = 
+    {
+      title: "Poikamiehen herkkuruoat",
+      author: "Anonyymi"
+    }
+    await api.post('/api/blogs')
+             .send(noUrl)
+             .expect(400)
+    const noTitleAndUrl =
+    {
+      author: "Anonyymi",
+      likes: 24
+    }
+    await api.post('/api/blogs')
+             .send(noTitleAndUrl)
+             .expect(400)
+  })
 })
 
-test('if no likes, set to zero', async () => {
-  const newBlog = 
-  { 
-    title: "Poikamiehen herkkuruoat",
-    author: "Anonyymi",
-    url: "https://poikamiehenherkkuruoat.tumblr.com/",
-  }
-  const response = await api.post('/api/blogs')
-                             .send(newBlog)
-                             .expect(201)
-                             .expect('Content-Type', /application\/json/)
-  expect(response.body.likes).toBe(0)
+describe('deleting blogs', () => {
+  test('deleting a blog, valid id', async () => {
+    await api.delete('/api/blogs/5a422bc61b54a676234d17fc')
+             .expect(204)
+    const allBlogs = await helper.blogsInDb()
+    expect(allBlogs).toHaveLength(helper.biglist.length - 1)
+  })
+
+  test('deleting a blog, invalid id', async () => {
+    await api.delete('/api/blogs/invalidid')
+             .expect(404)
+    const allBlogs = await helper.blogsInDb()
+    expect(allBlogs).toHaveLength(helper.biglist.length)
+  })
 })
 
-test.only('invalid parameters in adding a new blog', async () => {
-  const noTitle = 
-  { 
-    author: "Anonyymi",
-    url: "https://poikamiehenherkkuruoat.tumblr.com/",
-  }
-  await api.post('/api/blogs')
-           .send(noTitle)
-           .expect(400)
-  const noUrl = 
-  {
-    title: "Poikamiehen herkkuruoat",
-    author: "Anonyymi"
-  }
-  await api.post('/api/blogs')
-           .send(noTitle)
-           .expect(400)
-  const noTitleAndUrl =
-  {
-    author: "Anonyymi",
-    likes: 24
-  }
-  await api.post('/api/blogs')
-           .send(noTitleAndUrl)
-           .expect(400)
+describe('updating blogs', () => {
+  const blog = {
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 30
+}
+
+  test('updating a blog, valid id', async () => {
+    const updatedBlog = await api.put('/api/blogs/5a422bc61b54a676234d17fc')
+                                 .send(blog)
+                                 .expect(200)
+    expect(updatedBlog.body.likes).toBe(30)
+  })
+
+  test('updating a blog, invalid id', async () => {
+    await api.put('/api/blogs/invalidid')
+             .send(blog)
+             .expect(404)
+  })
 })
+
 
 afterAll(() => {
   mongoose.connection.close();
